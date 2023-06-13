62429f789834690600ccee5f4d03d1f791278301
Mikihisa Yuasa <myuasa2@illinois.edu>
2023-06-12 22:10:00 -0500

    feat: add chat gpt text remover

--- crawled_data_cleaning/cleaner.py
+++ crawled_data_cleaning/cleaner.py
@@ -1,4 +1,9 @@
+import os
 import re
+from typing import Literal
+
+from dotenv import load_dotenv
+import openai
 
 
 def remove_repeating_new_lines(text: str) -> str:
@@ -39,5 +44,52 @@ def remove_nav_texts(text: str) -> str:
     cleaned_text = re.sub(
         r"(CONTACT|TOP|ABOUT|BACK TO LIST|JOIN US|Contact|Top|About)", "", text
     )
+
+    return cleaned_text
+
+
+def remove_text_gpt(
+    text: str,
+    gpt_model: Literal["gpt-3.5-turbo", "gpt-4"] = "gpt-3.5-turbo",
+    openai_api_key: str | None = None,
+) -> str:
+    """
+    Removes unnecessary text from a string using OpenAI's GPT.
+
+    Parameters
+    ----------
+    text : str
+        Text to remove unnecessary text from
+    gpt_model : Literal["gpt-3.5-turbo", "gpt-4"] = "gpt-3.5-turbo"
+        GPT model to use
+    openai_api_key : str | None = None
+        OpenAI API key to use
+
+    Returns
+    -------
+    cleaned_text : str
+        Text with unnecessary text removed
+    """
+
+    if openai_api_key is None:
+        load_dotenv()
+        open_ai_api_key = os.getenv("OPENAI_API_KEY")
+        if open_ai_api_key is None:
+            raise ValueError(
+                "OpenAI API key not found. Please set OPEN_AI_API_KEY environment variable."
+            )
+        else:
+            print("Using OpenAI API key from environment variable.")
+    else:
+        print("Using OpenAI API key from function argument.")
+
+    prompt: str = f"以下の文から不要な要素を取り出してください。\n\n{text}"
+
+    response = openai.ChatCompletion.create(
+        model=gpt_model,
+        messages=[{"role": "user", "content": prompt}],
+    )
+
+    cleaned_text: str = response.choices[0].message.content
 
     return cleaned_text
--- tests/test_cleaners.py
+++ tests/test_cleaners.py
@@ -1,4 +1,8 @@
-from crawled_data_cleaning.cleaner import remove_repeating_new_lines, remove_nav_texts
+from crawled_data_cleaning.cleaner import (
+    remove_repeating_new_lines,
+    remove_nav_texts,
+    remove_text_gpt,
+)
 
 text: str = """【Innovation LAB】NTTPCのAIパートナープログラム
 
@@ -45,6 +49,18 @@ def test_remove_repeating_new_lines():
 def test_remove_nav_texts():
     log_path: str = "tmp/log_menu.txt"
     cleaned_text = remove_nav_texts(text)
+
+    with open(log_path, "w", encoding="utf-8") as f:
+        f.write(cleaned_text)
+
+    print(cleaned_text)
+
+    assert cleaned_text is not None
+
+
+def test_remove_text_gpt():
+    log_path: str = "tmp/log_gpt.txt"
+    cleaned_text = remove_text_gpt(text)
 
     with open(log_path, "w", encoding="utf-8") as f:
         f.write(cleaned_text)
