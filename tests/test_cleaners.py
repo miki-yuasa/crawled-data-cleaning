edb43b57bfe0aab618f904c7569fc1e4fdc652fb
Mikihisa Yuasa <myuasa2@illinois.edu>
2023-06-26 00:18:22 -0500

    feat: add text for tsukumin

--- tests/test_cleaners.py
+++ tests/test_cleaners.py
@@ -4,34 +4,10 @@ from crawled_data_cleaning.cleaner import (
     remove_text_gpt,
 )
 
-text: str = """【Innovation LAB】NTTPCのAIパートナープログラム
+text_file_path: str = "assets/texts/hikakin.txt"
 
-
-CONTACT
-
-
-TOP
-ABOUT
-PARTNERS
-CASE
-INTERVIEW
-JOIN US
-
-
-
-
-CASE活動実績
-BACK TO LIST
-
-
-
-おおいたAIテクノロジーセンター・NTTPC
-つくみんAIアイデアソン
-NTTPCは、2/22（水）、23（木）に津久見市で開催された『つくみんAIアイデアソン』にAIファシリテーターとして参加。
-22日は、マグロ加工工場、石灰鉱山・石灰製造現場、つくみイルカ島の飼育現場、みかん農家の生産・販売現場の4つの現場を視察するフィールドワークを実施。
-各コースには、業種・立場を問わずさまざまな参加者が入り、さまざまな視点を持って、AI利活用の課題解決や価値創造の種を生み出す企画。23日は、22日のフィールドワークを参考にして、AIアイデアソンを取り組み、AIアイデアを各チームより発表。
-
-開催日：2023年2月22日~23日"""
+with open(text_file_path, "r", encoding="utf-8") as f:
+    text: str = f.read()
 
 
 def test_remove_repeating_new_lines():
